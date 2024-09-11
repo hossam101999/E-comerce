9dbc906cb6ed6c2768a19685ea93b171e71701e7
FatmaYoussef3 <fatma.youssef.official@gmail.com>
2024-09-11 17:23:17 +0300

    FEAT:create user model (#4)

--- index.js
+++ index.js
@@ -18,22 +18,33 @@ app.use((err, req, res, next) => {
   }
 });
 
+//connect with mongodb
 mongoose.connect(process.env.DB_URL)
     .then(async () => {
 
         try {
-           
+            const { ADMIN_NAME, ADMIN_EMAIL, ADMIN_PASS } = process.env;
+            const existingAdmin = await User.findOne({ email: ADMIN_EMAIL });
+            if (!existingAdmin) {
+                const admin = new User({
+                    userName: ADMIN_NAME,
+                    email: ADMIN_EMAIL,
+                    password: ADMIN_PASS,
+                    isAdmin: true,
+                });
+                await admin.save();
+            }
             app.listen(process.env.PORT || PORT, () => {
                 console.log(`started with URL: http://localhost:${process.env.PORT || PORT}/`);
             })
         } catch (error) {
-            
+            logsFunction.error(` ${new Date().toISOString()} - Error: ${error.message}`);
             console.error('Error during database initialization:', error);
             process.exit(1);
         }
     })
     .catch(error => {
-       
+        logsFunction.error(` ${new Date().toISOString()} - Error: ${error.message}`);
         console.error('Database connection error:', error);
         process.exit(1);
     });
