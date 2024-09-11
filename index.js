2b0346a247263aefae220a360629ee5abfb8b103
Amr Mosad Kataria <159139472+Amrkataria@users.noreply.github.com>
2024-09-11 15:25:42 +0300

    FEAT:Connect with mongodb (#2)

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+require("dotenv").config();
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`App is running at http://localhost:${port}`);
-});
+
+mongoose.connect(process.env.DB_URL)
+    .then(async () => {
+
+        try {
+           
+            app.listen(process.env.PORT || PORT, () => {
+                console.log(`started with URL: http://localhost:${process.env.PORT || PORT}/`);
+            })
+        } catch (error) {
+            
+            console.error('Error during database initialization:', error);
+            process.exit(1);
+        }
+    })
+    .catch(error => {
+       
+        console.error('Database connection error:', error);
+        process.exit(1);
+    });
