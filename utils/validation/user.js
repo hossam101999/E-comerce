5a3a2e78042605145d8ae7a88528f46c14bd6215
Amr Mosad Kataria <159139472+Amrkataria@users.noreply.github.com>
2024-09-12 09:26:44 +0300

    FEAT:create validation updateProfileUser and changeUserPassword by joi (#10)

---
+++ utils/validation/user.js
@@ -0,0 +1,68 @@
+const Joi = require("joi");
+
+const updateProfile = Joi.object({
+  userName: Joi.string()
+    .required()
+    .pattern(/^[a-zA-Z\s]+$/)
+    .messages({
+      "string.base": "userName must be a string.",
+      "string.pattern.base":
+        "userName must only contain alphabetic characters and spaces.",
+      "string.empty": "userName is required.",
+      "any.required": "userName is required.",
+    }),
+  image: Joi.object({
+    buffer: Joi.binary().messages({
+      "binary.base": "Image file data must be provided",
+    }),
+    mimetype: Joi.string()
+      .regex(/^image\//)
+      .messages({
+        "string.base": "Image type must be a string",
+        "string.pattern.base": "Invalid image type",
+      }),
+  }).messages({
+    "object.base": "Image must be an object",
+  }),
+});
+const changePassword = Joi.object({
+  currentPassword: Joi.string()
+    .min(8)
+    .required()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/
+    )
+    .messages({
+      "string.base": "current password must be a string.",
+      "string.pattern.base":
+        "current password must contain at least one uppercase letter, one lowercase letter, one number, and one special character(@$!%*?).",
+      "string.min": "current password must be at least 8 characters long.",
+      "string.empty": "current password is required.",
+      "any.required": "current password is required.",
+    }),
+  newPassword: Joi.string()
+    .min(8)
+    .required()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/
+    )
+    .messages({
+      "string.base": "new password must be a string.",
+      "string.pattern.base":
+        "new password must contain at least one uppercase letter, one lowercase letter, one number, and one special character(@$!%*?).",
+      "string.min": "new password must be at least 8 characters long.",
+      "string.empty": "new password is required.",
+      "any.required": "new password is required.",
+    }),
+  confirmPassword: Joi.any().valid(Joi.ref("newPassword")).required().messages({
+    "any.only": "confirm passwords must match. with new password",
+    "any.required": "Confirm Password is required.",
+  }),
+});
+
+module.exports = {
+ 
+  changePassword,
+  updateProfile,
+
+};
