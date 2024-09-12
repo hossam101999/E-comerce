13b3698f0a8dea6fd3a691144d0aca5cf59479dd
Mohamed Heshsm <mohmedadm733+1@gmail.com>
2024-09-12 11:45:12 +0300

    FEAT:create alidation signup and login by joi (#18)

--- utils/validation/user.js
+++ utils/validation/user.js
@@ -1,5 +1,69 @@
 const Joi = require("joi");
 
+const createUser = Joi.object({
+  userName: Joi.string()
+      .required()
+      .pattern(/^[a-zA-Z\s]+$/)
+      .messages({
+          "string.base": "userName must be a string.",
+          "string.pattern.base":
+              "userName must only contain alphabetic characters and spaces.",
+          "string.empty": "userName is required.",
+          "any.required": "userName is required.",
+      }),
+  email: Joi.string().email().required().messages({
+      "string.base": "Email must be a string.",
+      "string.email": "Email must be a valid email address.",
+      "string.empty": "Email is required.",
+      "any.required": "Email is required.",
+  }),
+  password: Joi.string().min(8).required()
+      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/)
+      .messages({
+          "string.base": "Password must be a string.",
+          "string.pattern.base":
+              "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character(@$!%*?).",
+          "string.min": "Password must be at least 8 characters long.",
+          "string.empty": "Password is required.",
+          "any.required": "Password is required.",
+      }),
+  confirmPassword: Joi.any().valid(Joi.ref("password")).required().messages({
+      "any.only": "confirm passwords must match, with password",
+      "any.required": "Confirm Password is required.",
+  }),
+  image: Joi.object({
+      buffer: Joi.binary().messages({
+          'binary.base': 'Image file data must be provided',
+
+      }),
+      mimetype: Joi.string().regex(/^image\//).messages({
+          'string.base': 'Image type must be a string',
+          'string.pattern.base': 'Invalid image type',
+
+      }),
+
+  }).required()
+      .messages({
+          "any.required": "Image is required.",
+          'object.base': 'Image must be an object',
+
+      })
+});
+
+const loginUser = Joi.object({
+  email: Joi.string().email().required().messages({
+      "string.base": "Email must be a string.",
+      "string.email": "Email must be a valid email address.",
+      "string.empty": "Email is required.",
+      "any.required": "Email is required.",
+  }),
+  password: Joi.string().required()
+      .messages({
+          "string.base": "Password must be a string.",
+          "string.empty": "Password is required.",
+          "any.required": "Password is required.",
+      }),
+}); 
 const updateProfile = Joi.object({
   userName: Joi.string()
     .required()
@@ -61,7 +125,8 @@ const changePassword = Joi.object({
 });
 
 module.exports = {
- 
+  createUser,
+    loginUser,
   changePassword,
   updateProfile,
 
