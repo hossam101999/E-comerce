a85605c76a59f9d79e43206f6fa612900573a581
Mohamed Heshsm <mohmedadm733+1@gmail.com>
2024-09-12 10:22:40 +0300

    FEAT:create forget password and reset password and create nodemialer transporter (#12)

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const CustomError = require("../utils/customError");
 const jwt = require('jsonwebtoken');
+const transporter = require("../utils/nodemialer");
+const crypto = require("crypto");
 
 
 exports.signup = async (req, res, next) => {
@@ -86,5 +88,61 @@ exports.updateProfileUser = async (req, res, next) => {
       res.status(200).send({ message: "password updated successfully", user });
     } catch (error) {
       next(new CustomError(error.message, 500));
+    }
+  };
+
+  exports.forgetPassword = async (req, res, next) => {
+    const { email } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return next(new CustomError("User not found.", 404));
+      }
+  
+      const resetToken = crypto.randomBytes(3).toString("hex");
+      const resetTokenExpiration = Date.now() + 3600000;
+      user.resetToken = resetToken
+      user.resetTokenExpiration = resetTokenExpiration
+  
+      await user.save();
+  
+      const mailOptions = {
+        from: `Menha <${process.env.NODEMAILER_EMAIL}>`,
+        to: user.email,
+        subject: "Password Reset",
+        text: `This is your reset token: ${resetToken}`,
+      };
+  
+      await transporter.sendMail(mailOptions);
+  
+      res.status(200).send({ message: "Password reset email sent." });
+  
+    } catch (error) {
+        console.log(error)
+      next(new CustomError(error.message, 500));
+    }
+  };
+  exports.resetPassword = async (req, res, next) => {
+    const { token, email, newPassword } = req.body;
+  
+    try {
+  
+      const user = await User.findOne({
+        email,
+        resetToken: token,
+        resetTokenExpiration: { $gt: Date.now() },
+      });
+      if (!user) {
+        return next(new CustomError("rest token wrong or expirat or User not found.", 404));
+      }
+  
+      user.password = newPassword;
+      user.resetToken = undefined;
+      user.resetTokenExpiration = undefined;
+      await user.save();
+  
+      res.status(200).send({ message: "Password reset successfully" });
+    } catch (error) {
+      next(new CustomError("Internal server error.", 500));
     }
   };
